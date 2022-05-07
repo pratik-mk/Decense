18d86453f71b1b630a9c475eb92bf590998f5b13
Sahil Tembhare <sahil@rapidinnovation.dev>
2022-05-08 01:34:06 +0530

    resgistered all the mods

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+#[cfg(not(feature = "no-entrypoint"))]
+pub mod entrypoint;
+
+pub mod error;
+pub mod instruction;
+pub mod processor;
+pub mod state;
+pub mod validation;
