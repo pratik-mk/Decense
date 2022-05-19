fc5948c9f955b2708c31a923681c9a38562480e4
Sahil Tembhare <sahil@rapidinnovation.dev>
2022-05-20 01:09:29 +0530

    added InitializePlatform instruction

--- src/instruction.rs
+++ src/instruction.rs
@@ -3,6 +3,7 @@ use solana_program::program_error::ProgramError;
 use crate::error::DecenseError;
 
 pub enum DecenseInstruction {
+    InitializePlatform,
     Initialize { market_valuation: u64 },
 }
 
@@ -21,7 +22,8 @@ impl DecenseInstruction {
         let (ins_no, rest) = instruction_data.split_first().ok_or(DecenseError::InvalidInstruction)?;
         Ok(
             match ins_no {
-                0 => Self::Initialize {
+                0 => Self::InitializePlatform,
+                1 => Self::Initialize {
                     market_valuation: Self::get_number(rest)?
                 },
                 _ => return Err(DecenseError::InvalidInstruction.into())
