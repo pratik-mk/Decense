f9b55024d82e5d54a145ebfa1c7532f0a72d7da4
Sahil Tembhare <sahil@rapidinnovation.dev>
2022-05-24 00:54:53 +0530

    added a method to get the supply from the instruction data

--- src/instruction.rs
+++ src/instruction.rs
@@ -4,13 +4,22 @@ use crate::error::DecenseError;
 
 pub enum DecenseInstruction {
     InitializePlatform,
-    InitializeUser { market_valuation: u64 },
+    InitializeUser { market_valuation: u64, supply: u64 },
 }
 
 impl DecenseInstruction {
-    fn get_number(rest: &[u8]) -> Result<u64, ProgramError> {
+    fn get_market_valuation(rest: &[u8]) -> Result<u64, ProgramError> {
         let amount = rest
             .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(DecenseError::InvalidNumber)?;
+        Ok(amount)
+    }
+
+    fn get_supply(rest: &[u8]) -> Result<u64, ProgramError> {
+        let amount = rest
+            .get(8..16)
             .and_then(|slice| slice.try_into().ok())
             .map(u64::from_le_bytes)
             .ok_or(DecenseError::InvalidNumber)?;
@@ -24,7 +33,8 @@ impl DecenseInstruction {
         Ok(match ins_no {
             0 => Self::InitializePlatform,
             1 => Self::InitializeUser {
-                market_valuation: Self::get_number(rest)?,
+                market_valuation: Self::get_market_valuation(rest)?,
+                supply: Self::get_supply(rest)?
             },
             _ => return Err(DecenseError::InvalidInstruction.into()),
         })
