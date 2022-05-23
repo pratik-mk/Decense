26863fa18f9aa62fdc1cf40743b04f08e3383bbd
Sahil Tembhare <sahil@rapidinnovation.dev>
2022-05-24 00:49:36 +0530

    added new fields according to the changes

--- src/state.rs
+++ src/state.rs
@@ -58,11 +58,14 @@ pub struct UserState {
     pub is_initialized: bool,
     pub user: Pubkey,
     pub market_valuation: u64,
+    pub supply: u64,
     pub user_token_mint: Pubkey,
     pub user_ata: Pubkey,
     pub user_treasury_percentage: u8,
     pub liquidate_percentage: u8,
     pub pda_ata: Pubkey,
+    pub cmp: u64,
+    pub holders: u64,
 }
 
 impl Sealed for UserState {}
@@ -73,7 +76,7 @@ impl IsInitialized for UserState {
 }
 
 impl Pack for UserState {
-    const LEN: usize = 139;
+    const LEN: usize = 163;
 
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
         let src = array_ref![src, 0, UserState::LEN];
@@ -82,12 +85,15 @@ impl Pack for UserState {
             is_initialized,
             user,
             market_valuation,
+            supply,
             user_token_mint,
             user_ata,
             user_treasury_percentage,
             liquidate_percentage,
             pda_ata,
-        ) = array_refs![src, 1, 32, 8, 32, 32, 1, 1, 32];
+            cmp,
+            holders
+        ) = array_refs![src, 1, 32, 8, 8, 32, 32, 1, 1, 32, 8, 8];
 
         let is_initialized = match is_initialized {
             [0] => false,
@@ -99,11 +105,14 @@ impl Pack for UserState {
             is_initialized,
             user: Pubkey::new_from_array(*user),
             market_valuation: u64::from_le_bytes(*market_valuation),
+            supply: u64::from_le_bytes(*supply),
             user_token_mint: Pubkey::new_from_array(*user_token_mint),
             user_ata: Pubkey::new_from_array(*user_ata),
             user_treasury_percentage: user_treasury_percentage[0],
             liquidate_percentage: liquidate_percentage[0],
             pda_ata: Pubkey::new_from_array(*pda_ata),
+            cmp: u64::from_le_bytes(*cmp),
+            holders: u64::from_le_bytes(*holders)
         })
     }
 
@@ -114,31 +123,40 @@ impl Pack for UserState {
             is_initialized_dst,
             user_dst,
             market_valuation_dst,
+            supply_dst,
             user_token_mint_dst,
             user_ata_dst,
             user_treasury_percentage_dst,
             liquidate_percentage_dst,
             pda_ata_dst,
-        ) = mut_array_refs![dst, 1, 32, 8, 32, 32, 1, 1, 32];
+            cmp_dst,
+            holders_dst
+        ) = mut_array_refs![dst, 1, 32, 8, 8, 32, 32, 1, 1, 32, 8, 8];
 
         let UserState {
             is_initialized,
             user,
             market_valuation,
+            supply,
             user_token_mint,
             user_ata,
             user_treasury_percentage,
             liquidate_percentage,
             pda_ata,
+            cmp,
+            holders
         } = self;
 
         is_initialized_dst[0] = *is_initialized as u8;
         user_dst.copy_from_slice(user.as_ref());
-        *market_valuation_dst = market_valuation.to_be_bytes();
+        *market_valuation_dst = market_valuation.to_le_bytes();
+        *supply_dst = supply.to_le_bytes();
         user_token_mint_dst.copy_from_slice(user_token_mint.as_ref());
         user_ata_dst.copy_from_slice(user_ata.as_ref());
         user_treasury_percentage_dst[0] = *user_treasury_percentage;
         liquidate_percentage_dst[0] = *liquidate_percentage;
         pda_ata_dst.copy_from_slice(pda_ata.as_ref());
+        *cmp_dst = cmp.to_le_bytes();
+        *holders_dst = holders.to_le_bytes();
     }
 }
